4c9bee68c4c5b32fba3eafdcfc43422e0c8f0dca
kMed <kenta.medina@protonmail.com>
2022-05-31 20:32:52 +0900

    resolved errors; working on tutorial
    - fixed errors in main project
    - removed extra project (made for purpose of debugging earlier errors; no longer needed)

--- syncers_fps/Assets/BasicSpawner.cs
+++ syncers_fps/Assets/BasicSpawner.cs
@@ -1,14 +1,12 @@
+using System;
+using System.Collections.Generic;
 using Fusion;
 using Fusion.Sockets;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
+using UnityEngine.SceneManagement;
 
 public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
 {
-    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
-    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
     public void OnInput(NetworkRunner runner, NetworkInput input) { }
     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
@@ -53,6 +51,28 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
             {
                 StartGame(GameMode.Client);
             }
+        }
+    }
+
+    [SerializeField] private NetworkPrefabRef _playerPrefab;
+    private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        // Create a unique position for the player
+        Vector3 spawnPosition = new Vector3((player.RawEncoded%runner.Config.Simulation.DefaultPlayers)*3,1,0);
+        NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
+        // Keep track of the player avatars so we can remove it when they disconnect
+        _spawnedCharacters.Add(player, networkPlayerObject);
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        // Find and remove the players avatar
+        if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject))
+        {
+            runner.Despawn(networkObject);
+            _spawnedCharacters.Remove(player);
         }
     }
 }
