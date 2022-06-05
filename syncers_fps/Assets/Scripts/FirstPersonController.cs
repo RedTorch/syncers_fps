3a7da03a9e380882b0661adb607cce4932acd487
AoichiShisho <sonakamura.aoichi@gmail.com>
2022-06-05 13:22:40 +0900

    final commit for feature 1

--- syncers_fps/Assets/Scripts/FirstPersonController.cs
+++ syncers_fps/Assets/Scripts/FirstPersonController.cs
@@ -4,43 +4,100 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
-    // Playerが動く早さ
-    public float speed = 2.5f;
+    float x, z;
+    float speed = 0.1f;
 
-    public float jumpForce = 300.0f;
+    public GameObject cam;
+    Quaternion cameraRotation, charRotation;
+    float Xsensityvity = 3f, Ysensityvity = 3f;
+    
+    bool cursorLock = true;
+
+    //変数の宣言(角度の制限用)
+    float minX = -90f, maxX = 90f;
+
+    float jumpForce = 300.0f;
     public bool isJumping = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraRotation = cam.transform.localRotation;
+        charRotation = transform.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // left
-        if (Input.GetKey(KeyCode.A)) {
-            transform.position += Vector3.left * speed * Time.deltaTime;
-        }
-        // right
-        if (Input.GetKey(KeyCode.D)) {
-            transform.position += Vector3.right * speed * Time.deltaTime;
-        }
-        // forward
-        if (Input.GetKey(KeyCode.W)) {
-            transform.position += Vector3.forward * speed * Time.deltaTime;
-        }
-        // back
-        if (Input.GetKey(KeyCode.S)) {
-            transform.position += Vector3.back * speed * Time.deltaTime;
-        }
+        float xRotation = Input.GetAxis("Mouse X") * Ysensityvity;
+        float yRotation = Input.GetAxis("Mouse Y") * Xsensityvity;
+
+        cameraRotation *= Quaternion.Euler(-yRotation, 0, 0);
+        charRotation *= Quaternion.Euler(0, xRotation, 0);
+
+        //Updateの中で作成した関数を呼ぶ
+        cameraRotation = ClampRotation(cameraRotation);
+
+        cam.transform.localRotation = cameraRotation;
+        transform.localRotation = charRotation;
 
         // jump
         if (Input.GetKeyDown(KeyCode.Space) && isJumping == false) {
             this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
             isJumping = true;
         }
+
+        UpdateCursorLock();
+    }
+
+    private void FixedUpdate()
+    {
+        x = 0;
+        z = 0;
+
+        x = Input.GetAxisRaw("Horizontal") * speed;
+        z = Input.GetAxisRaw("Vertical") * speed;
+
+        transform.position += cam.transform.forward * z + cam.transform.right * x;
+    }
+
+
+    public void UpdateCursorLock()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cursorLock = false;
+        } else if (Input.GetMouseButton(0))
+        {
+            cursorLock = true;
+        }
+
+
+        if (cursorLock) 
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        } else if (!cursorLock) 
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    
+    //角度制限関数の作成
+    public Quaternion ClampRotation(Quaternion q)
+    {
+        //q = x,y,z,w (x,y,zはベクトル（量と向き）: wはスカラー（座標とは無関係の量）)
+
+        q.w = 1f;
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+
+        float angleX = Mathf.Atan(q.x) * Mathf.Rad2Deg * 2f;
+        angleX = Mathf.Clamp(angleX, minX, maxX);
+        q.x = Mathf.Tan(angleX * Mathf.Deg2Rad * 0.5f);
+
+        return q;
     }
 
     private void OnCollisionEnter(Collision collision) 
@@ -49,4 +106,5 @@ public class FirstPersonController : MonoBehaviour
             isJumping = false;
         }
     }
+
 }
